1a59eb4be788e178ba60ed2f096fa5fa56250bda
Shub Raj <hello@shubraj.com>
2022-12-15 19:57:04 +0545

    host changed

--- uploadTV.py
+++ uploadTV.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 import asyncio
 from getMeta import getSerie
 import datetime
-_base_url = "https://v15.dramaworldapp.xyz"
+_base_url = "https://v15b.dramaworldapp.xyz"
 _base_add_series = f"{_base_url}/admin/dashboard_api/add_web_series_api.php"
 _base_add_season = f"{_base_url}/admin/dashboard_api/add_season.php"
 _base_add_episode = f"{_base_url}/admin/dashboard_api/add_episode.php"
